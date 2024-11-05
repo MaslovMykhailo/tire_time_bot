c8c1efdfb0b83e097fcd3f07fa2ae8517e807a75
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2024-11-05 18:42:51 +0200

    Add comments describing the process of data storing

--- database/models.py
+++ database/models.py
@@ -11,13 +11,32 @@ class Base(DeclarativeBase):
     pass
 
 
+# When user submits location, the chat id, current tire type and coordinates are saved in the database.
+#
+# The bot should check the weather once a day and schedule alert of needed type when the weather changes.
+# The alert should be scheduled only when there are no other alerts for the same chat.
+# The alert counter changes every time when the alert is shown to the user.
+# When alert counter reaches 3, the alert is removed from the database and tire type is changed automatically.
+#
+# If the user react on alert, submits tire type change, then the alert should be removed from the database.
+# When the user changes current tire type, the alert should be removed from the database.
+
+
 class Chat(Base):
     __tablename__ = "chat"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     lat: Mapped[float] = mapped_column()
     lon: Mapped[float] = mapped_column()
-    alert: Mapped[List["Alert"]] = relationship(back_populates="chat")
+
+    tire_type: Mapped[int] = mapped_column()
+    formatted_tire_type = {
+        0: "winter",
+        1: "summer",
+    }
+
+    alerts: Mapped[List["Alert"]] = relationship(back_populates="chat")
 
     def __repr__(self) -> str:
         return f"Chat(id={self.id!r}, [lat={self.latitude!r}, lon={self.fullname!r}])"
